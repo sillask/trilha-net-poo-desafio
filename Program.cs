494a8637a4b41f59e8022a7af0845bdc0062e341
sillask <sillas-14@hotmail.com>
2024-01-04 14:30:16 -0300

    Desafio Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("Smartphone NOKIA");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "NOKIA C20", imei: "1212121212", memoria: 128);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone:");
+        Smartphone iphone = new Iphone(numero: "123456", modelo: "Iphone 14", imei: "2323232323", memoria: 256);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+
+        Console.ReadKey();
+    }
+}
